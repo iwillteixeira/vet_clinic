1072f2a9ca10f5cd3a1ae6c4ac0dde01025260a4
iwillteixeira <iwill2005@gmail.com>
2022-02-26 00:21:23 -0300

    Add table visits and specialiation

--- schema.sql
+++ schema.sql
@@ -29,4 +29,26 @@ ALTER TABLE animals ADD CONSTRAINT species_fk FOREIGN KEY (species_id) REFERENCE
 ALTER TABLE animals ADD owner_id INT;
 ALTER TABLE animals ADD CONSTRAINT owner_fk FOREIGN KEY (owner_id) REFERENCES owners;
 
+CREATE TABLE vets (
+   id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY NOT NULL,
+   name VARCHAR(50) NOT NULL,
+   age INT NOT NULL,
+   date_of_graduation DATE NOT NULL
+);
 
+CREATE TABLE specializations (
+   vets_id INT NOT NULL,
+   species_id INT NOT NULL,
+   FOREIGN KEY (vets_id) REFERENCES vets(id),
+   FOREIGN KEY (species_id) REFERENCES species(id),
+   PRIMARY KEY(vets_id,species_id)
+);
+
+CREATE TABLE visits (
+   animals_id INT NOT NULL,
+   vets_id INT NOT NULL,
+   date_of_visit DATE NOT NULL,
+   FOREIGN KEY (animals_id) REFERENCES animals(id),
+   FOREIGN KEY (vets_id) REFERENCES vets(id),
+   PRIMARY KEY (animals_id,vets_id, date_of_visit)
+);
